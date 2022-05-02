09f37b3d9d4b501ed6a1f45cafb156d0572a8ea0
anasseELQOUR <anasseelqour1996@gmail.com>
2022-05-02 03:25:36 +0000

    Registration Pages UI Designs : Donors and Recipients Activities

--- app/src/main/java/ma/ae/blooddonation/LoginActivity.java
+++ app/src/main/java/ma/ae/blooddonation/LoginActivity.java
@@ -1,13 +1,28 @@
 package ma.ae.blooddonation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registerButton = findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SelectRegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
