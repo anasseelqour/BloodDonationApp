7be92ae9d68a940425636e44886055e0dfebd921
anasseELQOUR <anasseelqour1996@gmail.com>
2022-05-15 23:26:46 +0100

    Send Email part 1 (add to libs : activation.jar additionnal.jar mail.jar and create JavaMail.java and Util.java)

---
+++ app/src/main/java/ma/ae/blooddonation/Email/Util.java
@@ -0,0 +1,8 @@
+package ma.ae.blooddonation.Email;
+
+public class Util {
+    // Add your own email and password over here.
+    public  static  final String EMAIL = "anasseelqour1996@gmail.com";
+    public static final String PASSWORD = "Hakima_1975";
+}
+
